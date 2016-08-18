ab8d9c7bd8ad3ccf6b0ac45cea1fa339668e6c61
nipa <nipa@codefx.org>
2016-08-18 18:02:22 +0200

    Demo absence of warnings on deprecated imports

---
+++ src/deprecated-imports/DeprecatedImports.java
@@ -0,0 +1,10 @@
+// in Java 8 this import causes a compiler warning
+// even though this type is deprecated itself
+import java.io.LineNumberInputStream;
+
+@Deprecated
+public class DeprecatedImports {
+
+	LineNumberInputStream stream;
+
+}
