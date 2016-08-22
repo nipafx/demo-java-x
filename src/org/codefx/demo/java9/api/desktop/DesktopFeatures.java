afdcc48b5fd15c022b44acb29139c4b68aff0034
nipa <nipa@codefx.org>
2016-08-22 12:17:34 +0200

    List platform-specific desktop integration

---
+++ src/org/codefx/demo/java9/api/desktop/DesktopFeatures.java
@@ -0,0 +1,21 @@
+package org.codefx.demo.java9.api.desktop;
+
+import java.awt.Taskbar;
+import java.awt.Taskbar.Feature;
+
+import static java.util.Arrays.stream;
+
+public class DesktopFeatures {
+
+	public static void main(String[] args) {
+		if (Taskbar.isTaskbarSupported()) {
+			System.out.println("Taskbar is supported - feature support breakdown:");
+			Taskbar taskbar = Taskbar.getTaskbar();
+			stream(Feature.values())
+					.forEach(feature -> System.out.printf(" - %s: %s%n", feature, taskbar.isSupported(feature)));
+		} else {
+			System.out.println("Taskbar is not on your platform. :(");
+		}
+	}
+
+}
