6b854957a1512b5470599f15a7e3ad984feb94d7
nipa <nipa@codefx.org>
2016-11-01 13:09:44 +0100

    Refactor 'java' call into run.sh

--- compile-run.sh
+++ compile-run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 ./compile.sh
-# --add-exports-private java.base/java.lang=ALL-UNNAMED
-# 	is needed for reflection over String internals in `Compaction`
-# -XX:-RestrictReservedStack
-#	is needed to unlock stack frame reservation in `ReservingStackAreas`
-$JAVA9_HOME/bin/java \
-	-cp out \
-	--add-exports-private java.base/java.lang=ALL-UNNAMED \
-	-XX:-RestrictReservedStack \
-	$1
+./run.sh $1
