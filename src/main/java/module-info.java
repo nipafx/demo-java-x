e4da990e218e88249f57452b263f927f1c9a56a7
Nicolai Parlog <nipa@codefx.org>
2022-03-13 12:35:20 +0100

    Create module declaration and fix modular build

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Project demonstrating various Java features introduced since Java 9.
+ */
+module org.codefx.demo.java_x {
+	exports org.codefx.demo.java18.jvm.javadoc;
+
+	requires java.desktop;
+	requires java.net.http;
+}
--- src/main/java/org/codefx/demo/java11/jvm/script/HelloJavaScripts.java
+++ src/main/java/org/codefx/demo/java11/jvm/script/HelloJavaScripts.java
@@ -1,3 +1,7 @@
+// package only needed to make this entire project compile on the module path;
+// your scripts absolutely don't have to belong to a package, so you can elide this line
+package org.codefx.demo.java11.jvm.script;
+
 public class HelloJavaScripts {
 
 	public static void main(String[] args) {
