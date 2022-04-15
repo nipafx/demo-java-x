5aacd7d96daeefe29743d0e89526ab50038453a0
Nicolai Parlog <nipa@codefx.org>
2022-04-15 10:02:06 +0200

    Demonstrate `Stream::toList`

---
+++ src/main/java/org/codefx/demo/java16/api/stream/ToList.java
@@ -0,0 +1,24 @@
+package org.codefx.demo.java16.api.stream;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ToList {
+
+	public static void main(String[] args) {
+		// create a list directly from stream, without collectors
+		List<String> letters = Stream
+				// these lists accept null
+				.of("A", "B", "C", null)
+				.toList();
+
+		// they are immutable, though
+		try {
+			letters.clear();
+			System.exit(1);
+		} catch (UnsupportedOperationException ex) {
+			System.out.println("Immutable stream-to-list: " + letters);
+		}
+	}
+
+}
